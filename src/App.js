86e9a8245f1070802ef8ac090d4e996f7fddda68
Septiana Yoga <septianayoga30@gmail.com>
2021-09-07 19:55:18 +0700

    adding slider data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import GlobalStyle from "./globalStyles";
 
@@ -7,7 +8,7 @@ function App() {
     <>
     <GlobalStyle />
       <Navbar />
-      <h1>Whatsapp</h1>
+      <Hero />
     </>
   );
 }
