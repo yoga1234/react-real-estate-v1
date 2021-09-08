92db51ef8ee11508f5c3ce533f6ce0565aa13722
Septiana Yoga <septianayoga30@gmail.com>
2021-09-08 16:45:56 +0700

    finishing project, the tutorial is not completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Dropdown from "./components/Dropdown";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import { SliderData } from "./data/SliderData";
 import GlobalStyle from "./globalStyles";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-    <GlobalStyle />
-      <Navbar />
+      <GlobalStyle />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
       <Hero slides={SliderData} />
     </>
   );
